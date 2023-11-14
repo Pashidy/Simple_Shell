ba6ef03546ea2118d789e64180d52e6bb99f29fd
Pashidy <eldafrancis@yahoo.com>
2023-11-14 09:51:15 -0800

    SIMPLE SHELL

---
+++ shell.h
@@ -0,0 +1,14 @@
+
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_SIZE 1024
+
+char *read_input(void);
+void process_input(char *input);
+
+#endif /* SHELL_H */
+
